d5b7dea30ab5862a43b7c40d70222920af43435d
VanDenHendeSimon <simonvdhende@outlook.com>
2020-10-06 10:49:26 +0200

    password toggle done

--- script/app.js
+++ script/app.js
@@ -1,9 +1,28 @@
+let passwordInput, passwordToggleInput, passwordIconShow, passwordIconHide;
+
 function handleFloatingLabel() {
    
 }
 
 function handlePasswordSwitcher() {
-    
+    // Fill global variables
+    passwordInput = document.querySelector('.js-password-input');
+
+    passwordToggleInput = document.querySelector('.js-password-toggle');
+    passwordIconShow = document.querySelector('.js-password-icon-show');
+    passwordIconHide = document.querySelector('.js-password-icon-hide');
+
+    passwordToggleInput.addEventListener("click", function() {
+        if (passwordToggleInput.checked) {
+            passwordIconShow.classList.add('c-password-toggle__icon--hidden');
+            passwordIconHide.classList.remove('c-password-toggle__icon--hidden');
+            passwordInput.type = "text";
+        }   else {
+            passwordIconHide.classList.add('c-password-toggle__icon--hidden');
+            passwordIconShow.classList.remove('c-password-toggle__icon--hidden');
+            passwordInput.type = "password";
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
